977b6139403f216897b1f07ded316498665864db
Przemysław Czajka <zamaskowany@o2.pl>
2022-11-01 16:42:26 +0100

    up

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import render_template
+from flask import request, redirect
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/mypage/me')
+def me():
+    return render_template('me.html')
+
+@app.route('/mypage/contact', methods=['GET', 'POST'])
+def contact():
+    return render_template('contact.html')
